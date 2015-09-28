825bc10bb9f6b10ef414347bfddd898acb247ced
Fekete Zoltán <Fekete Zoltn fekete.zoltan@minux.hu>
2015-09-28 15:42:28 +0200

    xlsx data import

--- controllers/daily_tour.py
+++ controllers/daily_tour.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import daily_tour_importer
 
 def index(): return dict(message="hello from daily_tour.py")
+
+
+def import_data():
+    import shutil
+    if request.vars:
+        filename = request.vars.upload.filename
+        file = request.vars.upload.file
+        filename = 'applications/FoodMaster/daily_tour_data/' + filename
+        shutil.copyfileobj(file, open(filename, 'wb'))
+
+    return dict(data=daily_tour_importer.show_importable_days(filename))
